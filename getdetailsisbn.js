961ace75dc584c67128c0bbbdc67d52dc0082fdd
Jeevanreddykankanala <121790173+Jeevanreddykankanala@users.noreply.github.com>
2023-09-15 14:10:51 +0530

    Update getdetailsisbn.js
    Implement a RESTful API endpoint that allows users to search for books by their International Standard Book Number (ISBN). Users can submit an ISBN as part of their request, and the API will respond with book details if a match is found. This task enhances the search functionality of the application, making it convenient for users to access specific books by their unique ISBNs

--- getdetailsisbn.js
+++ getdetailsisbn.js
@@ -3,19 +3,24 @@ const app = express();
 
 
 const books = [
-  { id: 1, title: 'Book 1', author: 'Author A' },
-  { id: 2, title: 'Book 2', author: 'Author B' },
-  { id: 3, title: 'Book 3', author: 'Author A' },
+  { id: 1, title: 'Book1', author: 'Author A', isbn: '1234567890' },
+  { id: 2, title: 'Book 2', author: 'Author B', isbn: '9876543210' },
+  { id: 3, title: 'Book 3', author: 'Author A', isbn: '5432109876' },
 ];
 
+app.get('/books/:isbn', (req, res) => {
+  const isbnToFind = req.params.isbn;
+  
+  const book = books.find(book => book.isbn === isbnToFind);
+  
+  if (!book) {
+    return res.status(404).json({ error: 'Book not found' });
+  }
 
-app.get('/books', (req, res) => {
-  // Return the list of books as a JSON response
-  res.json(books);
+  res.json(book);
 });
 
-
-const port = 4000; 
+const port = 4000; // 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
